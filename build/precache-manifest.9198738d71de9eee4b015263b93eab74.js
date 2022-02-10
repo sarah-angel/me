1b61cc9777d5e28cbf4405af4c6881ba5d3c0cba
sarah-angel <sarahangelk@gmail.com>
2022-02-10 15:30:38 +0300

    gitignore

---
+++ build/precache-manifest.9198738d71de9eee4b015263b93eab74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c545dff1156f50d259fc",
+    "url": "/me/static/js/main.12061b6a.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "40a336d489849ed4f650",
+    "url": "/me/static/css/2.15fb2160.chunk.css"
+  },
+  {
+    "revision": "40a336d489849ed4f650",
+    "url": "/me/static/js/2.5ca4bc4e.chunk.js"
+  },
+  {
+    "revision": "40cdf23766f6831b171311ca249f2061",
+    "url": "/me/static/media/bgr.40cdf237.jpg"
+  },
+  {
+    "revision": "35a9fcbbff5f21f9cc23530703a6ba3d",
+    "url": "/me/static/media/image_3.35a9fcbb.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/me/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/me/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/me/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "fda6836b3c53de7a04eb1dc67a5a226e",
+    "url": "/me/index.html"
+  }
+];
